0c8888cf123b387e2c8de5e72a2092965b138b0b
Sohail Iran <aratavalga@gmail.com>
2023-09-09 11:23:32 -0500

    exercise 7

--- reto.js
+++ reto.js
@@ -1,1 +1,47 @@
-let number;
+/* Ejercicio 1 */
+
+/* const number = prompt('Inserte su número.');
+
+if (number < 0) {
+    alert('Esto es un número negativo.');
+} else {
+    alert('Este es un número positivo.');
+} */
+
+/* Ejercicio 2 */
+
+/* let x = prompt('Ingrese número, para saber si es divisible entre 2');
+
+if (x % 2 === 0) {
+    alert('X número es divisible entre 2.');
+} else {
+    alert('x número no es divisible entre 2.');
+}
+ */
+
+/* Ejercicio 3 */
+
+/* const x = prompt('Ingrese primer número');
+const y = prompt('Ingrese segundo número');
+
+if (x > y) {
+    alert('El primer número es mayor.');
+} else {
+    alert('El segundo número es mayor.');
+} */
+
+/* Ejercicio 7 */
+
+const dia = prompt('Sú dia de la semana');
+
+if (dia === 'Lunes') {
+    alert('Su día es Lunes.');
+} else if (dia === 'Viernes') {
+    alert('Su día es Viernes.');
+} else if (dia === 'Sabado') {
+    alert('Su día es Sabado.');
+} else if (dia === 'Domingo') {
+    alert('Su día es Domingo.');
+} else {
+    alert('Ha digitado otra palabra.');
+}
