253527f285ffc732f695b4b534ddad7c4cd93a27
Sohail Iran <aratavalga@gmail.com>
2023-09-09 12:41:44 -0500

    agradecido

--- reto.js
+++ reto.js
@@ -21,18 +21,18 @@ if (x % 2 === 0) {
 
 /* Ejercicio 3 */
 
-/* const x = prompt('Ingrese primer número');
+const x = prompt('Ingrese primer número');
 const y = prompt('Ingrese segundo número');
 
 if (x > y) {
-    alert('El primer número es mayor.');
+    alert('El número ' + x + ' es mayor.');
 } else {
-    alert('El segundo número es mayor.');
-} */
+    alert('El número ' + y + ' es mayor.');
+}
 
 /* Ejercicio 7 */
 
-const dia = prompt('Sú dia de la semana');
+/* const dia = prompt('Sú dia de la semana');
 
 if (dia === 'Lunes') {
     alert('Su día es Lunes.');
@@ -44,4 +44,36 @@ if (dia === 'Lunes') {
     alert('Su día es Domingo.');
 } else {
     alert('Ha digitado otra palabra.');
-}
+} */
+
+/* Ejercicio 8 */
+
+/* const calificacion = prompt('Ingrese su nota promedia');
+
+if (calificacion == 10) {
+    alert('Excelente nota.');
+} else if (calificacion == 8) {
+    alert('Nota regular.');
+} else if (calificacion == 9) {
+    alert('Buena nota.');
+} else if (calificacion == 6) {
+    alert('Nota regular.');
+} */
+
+/* Ejercicio 11 */
+
+/* const year = prompt('Ingrese tus años de antiguedad');
+
+if (year == 1) {
+    alert('Su bono es de $100.');
+} else if (year == 2) {
+    alert('Su bono es de $200.');
+} else if (year == 3) {
+    alert('Su bono es de $300.');
+} else if (year == 4) {
+    alert('Su bono es de $400.');
+} else if (year == 5) {
+    alert('Su bono es de $500.');
+} else if (year > 5) {
+    alert('Su bono es de $1000.');
+} */
