600b94df8e4a991aa4a86a4da25e995eb71a599d
Sohail Iran <aratavalga@gmail.com>
2023-09-09 09:38:32 -0500

    reto personal

---
+++ main.js
@@ -0,0 +1,16 @@
+const a = 12;
+const b = 3;
+
+function AreaTriangulo() {
+    return (a * b) / 2;
+}
+
+function AreaCuadrado() {
+    return a * a;
+}
+
+console.log('---- Triangulo ----');
+console.log(AreaTriangulo());
+console.log('---- Cuadrado ----');
+console.log(AreaCuadrado());
+
---
+++ object.js
@@ -0,0 +1,9 @@
+const computer = {
+    monitor: 'Asus',
+    keyboard: 'Logitech',
+    CPU: 'AMD',
+    mouse: 'Genius'
+}
+
+console.log(JSON.stringify(computer));
+console.log(computer);
