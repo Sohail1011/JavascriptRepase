ee10b3eac5ada1befc0f9acf298ba2e5ba854252
Sohail Iran <aratavalga@gmail.com>
2023-10-15 22:42:48 -0500

    response added

--- consumer.js
+++ consumer.js
@@ -1,7 +1,20 @@
-async function readAPI() {
-    const request = await fetch(`https://nekos.best/api/v2/dance?amount=20`);
-    const response = await request.json();
-    console.log(response.results);
-}
+const _obtencion = document.getElementById('app');
 
+async function readAPI() {
+    const request = await fetch(`https://nekos.best/api/v2/kick?amount=20`);
+    const response = await request.json();
+    const data = response.results;
+    data.forEach((e) => {
+        const render = document.createElement('section');
+
+        render.className = 'recorte';
+
+        render.innerHTML = `
+            <h2>${e.anime_name}</h2>
+            <img src="${e.url}" alt="${e.anime_name}">
+        `;
+
+        _obtencion.appendChild(render);
+    })
+}
 readAPI();
